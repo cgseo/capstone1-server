cfb6143cf7ebf4bd372b3e30964a8109bb9253a9
cgs <cgyeongseo525@gmail.com>
2025-05-15 22:11:32 +0900

    noise-group member들의 noise조회 수정

--- dao/noise-dao.js
+++ dao/noise-dao.js
@@ -32,10 +32,33 @@ exports.getMaxDecibelsForMonth = async (userId, year, month) => {
 };
 
 // 특정 그룹에 속한 그룹원들의 최근 30분 이내 기록된 noise_log list 조회
+// 이거 보류..
+/*
 exports.getNoiseLogsByGroupId = async (groupId) => {
   const sql = 'SELECT `noise_log`.* FROM `group_members` INNER JOIN `noise_log` ' 
           + 'ON `group_members`.`user_id` = `noise_log`.`user_id` '
           + ' WHERE `group_id`=? AND `end_time` >= DATE_ADD(NOW(), INTERVAL -30 MINUTE)';
+  const [results] = await db.execute(sql, [groupId]);
+  return results;
+};
+*/
+
+// 특정 그룹에 속한 그룹원들의 최근 30분 이내 기록된 noise_log(noise_level, max_db) list
+// +members 정보(user_id, nickname, is_online) 조회
+exports.getNoiseLogsByGroupId = async (groupId) => {
+  // a == 특정 그룹에 속한 member들의 정보(user_id, nickname, is_online)
+  const sql = 'WITH `a` AS (' 
+	  +'SELECT `user_id`, `nickname`, `is_online` FROM `group_members` '
+	  +'INNER JOIN `users` ON `group_members`.`user_id` = `users`.`id` '
+	  +'WHERE `group_members`.`group_id` = ?)'
+    +', `ranked` as (SELECT `noise_level`, `max_db`, `user_id`, ' 
+    // ranked == 멤버별 최근 30분 이내에 기록된 noise_log들을 최신순으로 나열
+	  +'ROW_NUMBER() OVER (PARTITION BY `user_id` ORDER BY `end_time` DESC) AS `row_n` '
+	  +'FROM `noise_log` WHERE `end_time` >= NOW() - INTERVAL 30 MINUTE)'
+    // b == 멤버별 최근 30분 이내에 기론된 noise_log들 중 가장 최신 noise_log
+    +', `b` as (SELECT * FROM `ranked` WHERE `row_n` = 1 )'
+    // member 정보 + noise_log 정보 left join해서 list 조회
+ +'SELECT `a`.*, `b`.`noise_level`, `b`.`max_db` FROM `a` LEFT JOIN `b` ON `a`.`user_id`=`b`.`user_id`';
   const [results] = await db.execute(sql, [groupId]);
   return results;
 };
